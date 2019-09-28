2c5213bfbaf85cb7b262c4d9d7c666249057710d
Iryna Trush <iryna@onesignal.com>
2019-09-27 17:22:24 -0700

    Conveting Notification timestamp to number instead of string

--- src/service-worker/ServiceWorker.ts
+++ src/service-worker/ServiceWorker.ts
@@ -207,7 +207,7 @@ export class ServiceWorker {
               notificationId: notification.id,
               appId,
               url: notification.url,
-              timestamp: new Date().getTime().toString(),
+              timestamp: new Date().getTime(),
             }
             notificationReceivedPromises.push(Database.put("NotificationReceived", notificationReceived));
             // TODO: decide what to do with all the notif received promises
@@ -659,7 +659,7 @@ export class ServiceWorker {
         notificationId: notificationData.id,
         appId,
         url: launchUrl,
-        timestamp: new Date().getTime().toString(),
+        timestamp: new Date().getTime(),
       }
       Log.info("NotificationClicked", notificationClicked);
       saveNotificationClickedPromise = (async (notificationClicked) => {
